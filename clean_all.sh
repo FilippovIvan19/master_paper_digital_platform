f613773cd44496b0a4da982adceaa9b4e15aa45e
Ivan Filippov <fim1902@gmail.com>
2024-05-01 14:41:04 +0300

    improve starting and suspending scripts

--- clean_all.sh
+++ clean_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# you need sudo to run this script because of postgres service managing
+# do not use sudo to run this script, it will cause issues with unavailability of some service directories (like logs, tmp, pids, etc)
 
 rm out
 
 cd $KAFKA_HOME
 # stop kafka services
-bin/zookeeper-server-stop.sh config/zookeeper.properties
 bin/kafka-server-stop.sh config/server.properties
+bin/zookeeper-server-stop.sh config/zookeeper.properties
 
 cd $HIVE_HOME
 pgrep -f org.apache.hadoop.hive.metastore.HiveMetaStore | xargs -r kill
@@ -19,10 +19,10 @@ rm    derby.log
 rm -r metastore_db
 
 cd $HADOOP_HOME
-sbin/stop-dfs.sh
+sbin/stop-dfs.sh > /dev/null 2> /dev/null
 # clear incorrect state
 rm -r /tmp/hadoop-*
-bin/hdfs namenode -format -force
+bin/hdfs namenode -format -force > /dev/null 2> /dev/null
 
 pgrep -f NilmAnalyticsMlApp/.venv/bin/flask | xargs -r kill
 
--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# you need sudo to run this script because of postgres service managing
+# do not use sudo to run this script, it will cause issues with unavailability of some service directories (like logs, tmp, pids, etc)
 
 SCRIPT_DIR=$(pwd)
 RED='\033[0;31m'
@@ -10,6 +10,7 @@ OUT_FILE=$SCRIPT_DIR/out
 cd $KAFKA_HOME
 # start kafka services
 nohup bin/zookeeper-server-start.sh config/zookeeper.properties > $OUT_FILE 2> $OUT_FILE &
+sleep 20
 nohup bin/kafka-server-start.sh config/server.properties > $OUT_FILE 2> $OUT_FILE &
 sleep 2
 # kafka consumer terminal window
@@ -17,9 +18,9 @@ x-terminal-emulator -e bin/kafka-console-consumer.sh --bootstrap-server localhos
 echo -e "${RED}KAFKA FINISHED${NC}"
 
 cd $HADOOP_HOME
-sbin/start-dfs.sh
+sbin/start-dfs.sh > $OUT_FILE 2> $OUT_FILE
 # terminal window for running hdfs commands
-x-terminal-emulator
+# x-terminal-emulator
 echo -e "${RED}HADOOP FINISHED${NC}"
 
 cd $DERBY_HOME
@@ -32,12 +33,12 @@ echo -e "${RED}DERBY FINISHED${NC}"
 
 cd $HIVE_HOME
 # init metastore database
-bin/schematool -dbType derby -initSchema -url jdbc:derby://127.0.0.1:1527/metastore_db -driver org.apache.derby.jdbc.ClientDriver
+bin/schematool -dbType derby -initSchema -url jdbc:derby://127.0.0.1:1527/metastore_db -driver org.apache.derby.jdbc.ClientDriver > $OUT_FILE 2> $OUT_FILE
 # start metastore server for multiple connections
 nohup bin/hive --service metastore > $OUT_FILE 2> $OUT_FILE &
 sleep 2
 # create hive table
-bin/beeline -u jdbc:hive2:// --showDbInPrompt=true < $SCRIPT_DIR/hive_init.sql
+bin/beeline -u jdbc:hive2:// --showDbInPrompt=true < $SCRIPT_DIR/hive_init.sql > $OUT_FILE 2> $OUT_FILE
 # terminal window with beeline for hive
 x-terminal-emulator -e bin/beeline -u jdbc:hive2:///smartmonitoring --showDbInPrompt=true
 echo -e "${RED}HIVE FINISHED${NC}"
